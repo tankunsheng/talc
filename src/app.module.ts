9cf810ab10c16420e6640620fd40c6552bbd7c6b
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-02-08 01:44:18 +0800

    Add orm config in app module. Change npm run start:prod to target main in src

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Business } from './entities/business.entity';
 import { User } from './entities/user.entity';
-
+import { BusinessModule } from './business/business.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,23 +17,17 @@ import { User } from './entities/user.entity';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname),
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DB_HOST,
-    //   port: 5432,
-    //   username: process.env.DB_USER,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_NAME,
-    //   entities: [Business, User],
-    //   synchronize: true,
-    //   migrationsTableName: 'migration_table',
-    //   migrations: ['migration/*.js'],
-    //   cli: {
-    //     migrationsDir: 'migration',
-    //   },
-    // }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Business, User],
+    }),
     AuthModule,
+    BusinessModule,
   ],
   controllers: [AppController],
   providers: [AppService],
