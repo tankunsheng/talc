f3cceae271adcc36daa7b779ed1197d27bfcd333
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-02-08 01:49:25 +0800

    Remove non existent file in import

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Business } from './entities/business.entity';
 import { User } from './entities/user.entity';
-import { BusinessModule } from './business/business.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
